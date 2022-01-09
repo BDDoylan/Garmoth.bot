c8fbab6efb24b8875dce4d009396bf60111e5556
JoachimGoodman <44166049+JoachimGoodman@users.noreply.github.com>
2022-01-09 22:34:24 +0100

    env, db, package

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
-import mysql from 'mysql'
+// Accessing .env
+import dotenv from "dotenv";
+dotenv.config();
+
+import mysql from 'mysql2'
+
 export default mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "password",
-    database: "garmoth"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
 });
 
